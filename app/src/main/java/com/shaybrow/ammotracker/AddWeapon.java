2ee1a94c136b25579bf36c663cc73635501ef93a
Seamus Brown <shay_brown_13@hotmail.com>
2021-06-17 17:44:14 -0700

    room integration, initial setup

---
+++ app/src/main/java/com/shaybrow/ammotracker/AddWeapon.java
@@ -0,0 +1,13 @@
+package com.shaybrow.ammotracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddWeapon extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
