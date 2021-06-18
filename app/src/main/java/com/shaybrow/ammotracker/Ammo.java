f67605f662c7766f8b4f7c9e106aceb0eaae20f1
Seamus Brown <shay_brown_13@hotmail.com>
2021-06-17 18:21:23 -0700

    ammo and weapons first pass

---
+++ app/src/main/java/com/shaybrow/ammotracker/Ammo.java
@@ -0,0 +1,8 @@
+package com.shaybrow.ammotracker;
+
+public class Ammo {
+    protected int clipSize;
+    private String damageType;
+    private int totalShots;
+
+}
---
+++ app/src/main/java/com/shaybrow/ammotracker/Weapon.java
@@ -0,0 +1,10 @@
+package com.shaybrow.ammotracker;
+
+import java.util.List;
+
+public class Weapon {
+    private String name;
+    private List<Ammo> ammoList;
+    private List<Integer> shootAmount;
+    
+}
