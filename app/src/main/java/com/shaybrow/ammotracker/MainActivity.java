74cd019680a6777c47db12d247510c12df713aed
Seamus Brown <shay_brown_13@hotmail.com>
2021-07-24 17:49:34 -0700

    home page updated

--- app/src/main/java/com/shaybrow/ammotracker/MainActivity.java
+++ app/src/main/java/com/shaybrow/ammotracker/MainActivity.java
@@ -9,5 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getMenuInflater().inflate(R);
     }
 }
